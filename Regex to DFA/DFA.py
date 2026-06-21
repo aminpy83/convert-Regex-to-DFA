ddfde0a02cc19bac2074900ac336e58d9f766dd2
aminpy83 <lifelikesamin@gmail.com>
2026-06-21 18:40:40 +0330

    DFA adding converter (nfa_to_dfa)

--- Regex to DFA/DFA.py
+++ Regex to DFA/DFA.py
@@ -42,3 +42,23 @@ def move(states, symbol):
             result_states.update(state.transitions[symbol])
 
     return result_states
+
+def nfa_to_dfa(nfa, alphabet):
+    start_closure = epsilon_closure(nfa.start)
+    unmarked_states = [start_closure]
+    dfa_states = {frozenset(start_closure): State()} # using immutable set for avoiding repetitive sets
+
+    while unmarked_states:
+        state = unmarked_states.pop()
+        for character in alphabet:
+            move_result = move(state, character)
+            next_closure = epsilon_closure(move_result)
+
+            next_closure_frozen = frozenset(next_closure)
+
+            if next_closure_frozen not in dfa_states:
+                dfa_states[next_closure_frozen] = State() # making new dfa with unmarks
+                unmarked_states.append(next_closure)
+
+            dfa_states[frozenset(state)].add_transition(character, dfa_states[next_closure_frozen])
+
